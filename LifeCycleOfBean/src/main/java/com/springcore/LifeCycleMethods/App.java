feb8a50c41585574ea67af59161d34ca72dd8953
PrachiJainTricon <prachi.jain@triconinfotech.com>
2023-04-12 12:31:24 +0530

    Life cycle methods of spring bean

---
+++ LifeCycleOfBean/src/main/java/com/springcore/LifeCycleMethods/App.java
@@ -0,0 +1,13 @@
+package com.springcore.LifeCycleMethods;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
