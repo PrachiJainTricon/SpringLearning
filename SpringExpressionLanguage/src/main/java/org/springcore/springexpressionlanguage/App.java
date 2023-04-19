5199efa8517034c70b82d8a674f78bdc143344db
PrachiJainTricon <prachi.jain@triconinfotech.com>
2023-04-19 16:06:22 +0530

    invoke static method and variable , create object using SpEL

--- SpringExpressionLanguage/src/main/java/org/springcore/springexpressionlanguage/App.java
+++ SpringExpressionLanguage/src/main/java/org/springcore/springexpressionlanguage/App.java
@@ -2,6 +2,8 @@ package org.springcore.springexpressionlanguage;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 /**
  * Hello world!
@@ -12,20 +14,14 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("springExpressionConfig.xml");
+        Demo d1 = context.getBean("demo" , Demo.class);
+        System.out.println(d1);
 
-        System.out.println("_________________Prototype Scope________________");
-        Student student = context.getBean("ob" ,Student.class);
-        System.out.println(student.hashCode()); // to get hashcode of bean : 360067785
+        // expression using
+        SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
+        Expression expression = spelExpressionParser.parseExpression("2 +74"); // direct pass the expression
+        System.out.println(expression.getValue());
 
-        Student student1 = context.getBean("ob",Student.class);
-        System.out.println(student1.hashCode()); // 360067785 NOTE : hashcode is same , meaning it will give same object
 
-//      prototype scope using xml
-        Teacher teacher = context.getBean("teacher" ,Teacher.class);
-        Teacher teacher1 = context.getBean("teacher" , Teacher.class);
-        System.out.println(teacher.hashCode()); //1436664465
-        System.out.println(teacher1.hashCode()); //558187323
-
-        System.out.println( "Hello World!" );
     }
 }
