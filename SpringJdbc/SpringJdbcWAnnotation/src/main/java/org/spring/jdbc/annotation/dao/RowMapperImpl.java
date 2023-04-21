83a2ccacd2eb4b77d57ee796c319280ac0504e3f
PrachiJainTricon <prachi.jain@triconinfotech.com>
2023-04-21 11:39:20 +0530

    bug

---
+++ SpringJdbc/SpringJdbcWAnnotation/src/main/java/org/spring/jdbc/annotation/dao/RowMapperImpl.java
@@ -0,0 +1,21 @@
+package org.spring.jdbc.annotation.dao;
+
+
+import org.spring.jdbc.annotation.entity.Student;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RowMapperImpl implements RowMapper<Student> {
+    @Override
+    public Student mapRow(ResultSet resultSet, int i) throws SQLException {
+        Student student = new Student();
+        student.setId(resultSet.getInt(1)); // getInt(column number)
+        student.setName(resultSet.getString(2));
+        student.setCity(resultSet.getString(3));
+
+        return student;
+    }
+}
